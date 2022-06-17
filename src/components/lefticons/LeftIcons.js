6193f3bd392e5aa838011a5468edc3c09d0f15a2
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-17 17:21:52 +0300

    icons

--- src/components/lefticons/LeftIcons.js
+++ src/components/lefticons/LeftIcons.js
@@ -4,7 +4,7 @@ import "./LeftIconStyle.css";
 const LeftIcons = () => {
   return (
     <div className="icons">
-      <div className="icons-menu mb-5">
+      <div className="icons-menu mb-5 ">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="30"
@@ -32,7 +32,7 @@ const LeftIcons = () => {
           <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
           <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z" />
         </svg>
-        <p>All Chats</p>
+        <p className="text-white">All Chats</p>
       </div>
 
       <div className="icons-group">
@@ -51,7 +51,7 @@ const LeftIcons = () => {
           />
           <path d="M4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z" />
         </svg>
-        <p>Group</p>
+        <p className="text-white">Group</p>
       </div>
 
       <div className="icons-shaxsiy">
@@ -69,7 +69,7 @@ const LeftIcons = () => {
             d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
           />
         </svg>
-        <p>Shaxsiy</p>
+        <p className="text-white">Shaxsiy</p>
       </div>
 
       <div className="icons-channel">
@@ -83,7 +83,7 @@ const LeftIcons = () => {
         >
           <path d="M8 1a5 5 0 0 0-5 5v1h1a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V6a6 6 0 1 1 12 0v6a2.5 2.5 0 0 1-2.5 2.5H9.366a1 1 0 0 1-.866.5h-1a1 1 0 1 1 0-2h1a1 1 0 0 1 .866.5H11.5A1.5 1.5 0 0 0 13 12h-1a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1h1V6a5 5 0 0 0-5-5z" />
         </svg>
-        <p>Channel</p>
+        <p className="text-white">Channel</p>
       </div>
 
       <div className="icons-oqilmagan">
@@ -97,7 +97,7 @@ const LeftIcons = () => {
         >
           <path d="M16 8c0 3.866-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7zM5 8a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm4 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
         </svg>
-        <p>Reading</p>
+        <p className="text-white">Reading</p>
       </div>
 
       <div className="icons-edit">
@@ -114,7 +114,7 @@ const LeftIcons = () => {
             d="M11.5 2a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM9.05 3a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0V3h9.05zM4.5 7a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM2.05 8a2.5 2.5 0 0 1 4.9 0H16v1H6.95a2.5 2.5 0 0 1-4.9 0H0V8h2.05zm9.45 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm-2.45 1a2.5 2.5 0 0 1 4.9 0H16v1h-2.05a2.5 2.5 0 0 1-4.9 0H0v-1h9.05z"
           />
         </svg>
-        <p>Edit</p>
+        <p className="text-white">Edit</p>
       </div>
     </div>
   );
